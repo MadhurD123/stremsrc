887a5fafbbf2ac606f799fbc799cd3c085960065
MadhurD123 <madhur.devakki@gmail.com>
2025-11-01 22:33:31 +0530

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const TMDB_META_URL = "https://94c8cb9f702d-tmdb-addon.baby-beamup.club/meta";
-export const SOURCE_URL = "https://vidsrc-embed.su/embed";
+export const SOURCE_URL = "https://vsrc.su/embed";
